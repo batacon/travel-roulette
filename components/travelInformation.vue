76b62950cc96f30159011a712c47c3371b84691a
Kawarabata <kwrbt0404@gmail.com>
2021-08-27 10:56:35 +0900

    create roulette

---
+++ components/travelInformation.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="travel-information">
+    <p>{{ travel.name }}といえば...</p>
+    <ul>
+      <li v-for="(spot, index) in travel.spots" :key="index">{{ spot }}とか</li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    travel: {
+      type: Object,
+      required: true,
+    },
+  },
+})
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,39 @@
 <template>
-  <Tutorial/>
+  <div class="roulette">
+    <h1>国内旅行先ルーレット</h1>
+    <button @click="spinRoulette">旅行先は？</button>
+    <h2 v-if="showTravel">{{ selectedTravel.name }}だ！</h2>
+    <travel-information v-if="showTravel" :travel="selectedTravel" />
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import travels from '../assets/json/travels.json'
 
-export default Vue.extend({})
+export default Vue.extend({
+  data() {
+    const showTravel: boolean = false
+    const selectedTravel: any = {}
+
+    return {
+      showTravel,
+      selectedTravel,
+    }
+  },
+  methods: {
+    spinRoulette() {
+      const randomIndex = Math.floor(Math.random() * travels.length)
+      this.selectedTravel = travels[randomIndex]
+      this.showTravel = true
+    },
+  },
+})
 </script>
+
+<style>
+.roulette {
+  width: 400px;
+  margin: 0 auto;
+}
+</style>
